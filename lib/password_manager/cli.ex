f3a399d6dbda0fb1502ee996125bdd3c0b324c24
Barry Andrews <bandrews@Barrys-Mac-mini.local>
2015-03-21 23:43:46 -0400

    Added help and exit to CLI

--- lib/password_manager/cli.ex
+++ lib/password_manager/cli.ex
@@ -1,7 +1,48 @@
 defmodule PasswordManager.CLI do
 
-  def main(argv) do
-    IO.puts "Welcome to Password Manager!"
+  defmodule CmdProcessor do
+    def accept_cmd("help") do
+      _help
+    end
+
+    def accept_cmd("h") do
+      _help
+    end
+
+    def accept_cmd("exit") do
+      _exit
+    end
+
+    def accept_cmd("x") do
+      _exit
+    end
+
+    def accept_cmd(x) do
+      IO.puts "Don't know how to #{x}\nType help to get help."
+    end
+
+    defp _help do
+      IO.puts "Commands are:"
+      IO.puts "\t(h)elp"
+      IO.puts "\te(x)it"
+    end
+
+    defp _exit do
+      IO.puts "Goodbye!"
+      System.halt
+    end
   end
 
+  def process_input do
+    input = IO.gets(:standard_io, "?") |> String.downcase |> String.strip
+    CmdProcessor.accept_cmd(input)
+    process_input
+  end
+
+  def main(argv) do
+    IO.puts "\n\t\tWelcome to Password Manager!\n"
+    File.read!("LICENSE") |> IO.puts
+    IO.puts "\tEnter help to see a list of commands"
+    process_input
+  end
 end
